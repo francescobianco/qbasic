d550e9319421be683db8f1ea4a0f9997f88118f3
francescobianco <info.francescobianco@gmail.com>
2017-04-24 02:40:08 +0200

    try twitter features

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 
 var gulp = require('gulp');
 var concatCss = require('gulp-concat-css');
+var modifyCssUrls = require('gulp-modify-css-urls');
 
 gulp.task("default", function() {
     gulp.src("./node_modules/**/css/*.css")
         .pipe(concatCss("style.css"))
+        .pipe(modifyCssUrls({ modify: function (url, filePath) { return '../fonts/' + url; } }))
         .pipe(gulp.dest("./css"));
     gulp.src("./node_modules/**/fonts/*.{ttf,woff,eof,eot,svg,htc}")
         .pipe(gulp.dest("./fonts"));
